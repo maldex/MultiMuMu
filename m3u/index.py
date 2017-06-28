96ee0332f1480063e14437ea80aaf699a3fe1672
user@DVBt-2017-06 <user@dvb-t.localdomain>
2017-06-29 00:45:13 +0200

    ...

---
+++ m3u/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, glob
+from glob import glob
+
+if __name__ == "__main__":
+    print "Content-Disposition: attachment; filename=\"m3u.m3u\"" + os.linesep
+    print '#EXTM3U'
+    for m3u in glob('*.m3u'):
+        with open(m3u,'r') as f:
+            for l in f.readlines():
+                if not l.startswith('#EXTM3U'):
+                    print l.strip()
